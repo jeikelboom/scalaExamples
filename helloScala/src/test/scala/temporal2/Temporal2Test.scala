693eb2ebd27059587354f14ad039d3646edd9547
johaneikelboom <johan@jeikelboom.eu>
2020-12-10 20:13:00 +0100

    testdata

--- helloScala/src/test/scala/temporal2/Temporal2Test.scala
+++ helloScala/src/test/scala/temporal2/Temporal2Test.scala
@@ -1,33 +1,11 @@
 package temporal2
 
-import java.time.Instant
-
 import org.scalatest.{FlatSpec, Matchers}
-import temporal2.Intervals.{Interval, read}
 import temporal2.Temporal2._
+import TestData._
 
 
 class Temporal2Test  extends FlatSpec with Matchers {
-
-
-  val t0 :Instant = Instant.MIN
-  val t9 :Instant = Instant.MAX
-  val t1 :Instant = read("2011-04-01 10:10")
-  val t2 :Instant = read("2012-04-01 11:11")
-  val t3 :Instant = read("2013-04-01 12:12")
-  val t4 :Instant = read("2014-04-01 13:13")
-  val t5 :Instant = read("2015-04-01 14:14")
-  val i12: Interval = Interval(t1, t2)
-  val i13: Interval = Interval(t1, t3)
-  val i45: Interval = Interval(t4, t5)
-  val i23: Interval = Interval(t2, t3)
-  val i01: Interval = Interval(t0, t1)
-  val i04: Interval = Interval(t0, t4)
-  val i09: Interval = Interval(t0, t9)
-  val i19: Interval = Interval(t1, t9)
-  val i29: Interval = Interval(t2, t9)
-  val i34: Interval = Interval(t3, t4)
-  val i39: Interval = Interval(t3, t9)
 
   "timeline" should "build from open Intervals" in {
     val timeline: TimeLine[String] = TimeLine(List())
---
+++ helloScala/src/test/scala/temporal2/TestData.scala
@@ -0,0 +1,29 @@
+package temporal2
+
+import java.time.Instant
+
+import temporal2.Intervals.{Interval, read}
+
+object TestData {
+
+  val t0 :Instant = Instant.MIN
+  val t9 :Instant = Instant.MAX
+  val t1 :Instant = read("2011-04-01 10:10")
+  val t2 :Instant = read("2012-04-01 11:11")
+  val t3 :Instant = read("2013-04-01 12:12")
+  val t4 :Instant = read("2014-04-01 13:13")
+  val t5 :Instant = read("2015-04-01 14:14")
+  val i12: Interval = Interval(t1, t2)
+  val i13: Interval = Interval(t1, t3)
+  val i45: Interval = Interval(t4, t5)
+  val i23: Interval = Interval(t2, t3)
+  val i01: Interval = Interval(t0, t1)
+  val i04: Interval = Interval(t0, t4)
+  val i09: Interval = Interval(t0, t9)
+  val i19: Interval = Interval(t1, t9)
+  val i29: Interval = Interval(t2, t9)
+  val i34: Interval = Interval(t3, t4)
+  val i39: Interval = Interval(t3, t9)
+
+
+}
