a9ff2faf6a304d6f05ea24ed5c4adb969ead00e9
johaneikelboom <johan@jeikelboom.eu>
2020-12-28 14:39:16 +0100

    tests

--- helloScala/src/main/scala/temporal2/DiscreteTemporal.scala
+++ helloScala/src/main/scala/temporal2/DiscreteTemporal.scala
@@ -5,35 +5,19 @@ import java.time.LocalDate
 import cats.collections.{Discrete, Range}
 import cats.{Applicative, Order}
 
+import scala.annotation.tailrec
+
 object
 DiscreteTemporal {
-
-  implicit def localDateEnum: Discrete[LocalDate] = new Discrete[LocalDate] {
-    override def succ(x: LocalDate): LocalDate = x.plusDays(1)
-
-    override def pred(x: LocalDate): LocalDate = x.minusDays(1)
-  }
-
-  implicit def localDateOrder: Order[LocalDate] = (x: LocalDate, y: LocalDate) => x.compareTo(y)
-
-  def minus[A](a: Range[A], b: Range[A])(implicit enum: Discrete[A], order: Order[A]): (Option[Range[A]], Option[Range[A]]) = {
-    val left = if (order.gt(b.start, a.start)) Some(Range(a.start, order.min(a.end, enum.pred(b.start)))) else None
-    val right = if (order.lt(b.end, a.end)) Some(Range(order.max(a.start, enum.succ(b.end)), a.end)) else None
-    (left, right)
-  }
-
-  def date(year: Int, month: Int, day: Int): LocalDate = LocalDate.of(year, month, day)
 
   case class TimelineElement[A](start: LocalDate, end: LocalDate, value: A) {
     val range: Range[LocalDate] = Range(start, end)
 
     def append(that: TimelineElement[A]): List[TimelineElement[A]] = {
       if (this.value == that.value) {
-        val diff = this.range.+(that.range)
-        (diff._1 :: diff._2.toList).map(TimelineElement(_, value))
+        plus(this.range, that.range).map(TimelineElement(_, value))
       } else {
-        val diff = minus(this.range, that.range)
-        that :: diff._1.toList.map(TimelineElement(_, this.value))
+        that :: minus(this.range, that.range)._1.toList.map(TimelineElement(_, this.value))
       }
     }
 
@@ -63,18 +47,20 @@ DiscreteTemporal {
     override def toString: String = history.map(elt => s"$elt\n").fold("")((x, y) => x.concat(y))
   }
 
+  object DiscreteTimeline{
+    def apply[A](): DiscreteTimeline[A] = new DiscreteTimeline(List())
+  }
+
 
   implicit def timeLineApplicative: Applicative[DiscreteTimeline] = new Applicative[DiscreteTimeline] {
 
     override def pure[A](x: A): DiscreteTimeline[A] = new DiscreteTimeline[A](List(TimelineElement(Range(LocalDate.MIN, LocalDate.MAX), x)))
 
     override def ap[A, B](ff: DiscreteTimeline[A => B])(fa: DiscreteTimeline[A]): DiscreteTimeline[B] = {
-      val ffList = ff.history.reverse
-      val faList = fa.history.reverse
-      accumulator(ffList, faList, DiscreteTimeline[B](List()))
+      accumulator(ff.history.reverse, fa.history.reverse, DiscreteTimeline[B](List()))
     }
 
-    @scala.annotation.tailrec
+    @tailrec
     private def accumulator[A, B](ff: List[TimelineElement[A => B]], fa: List[TimelineElement[A]], accu: DiscreteTimeline[B]): DiscreteTimeline[B] = {
       (ff, fa) match {
         case ((ff@TimelineElement(_, _, _)) :: fftail, (fa@TimelineElement(_, _, _)) :: fatail) =>
@@ -88,5 +74,26 @@ DiscreteTemporal {
     }
 
   }
+
+  implicit def localDateEnum: Discrete[LocalDate] = new Discrete[LocalDate] {
+    override def succ(x: LocalDate): LocalDate = x.plusDays(1)
+
+    override def pred(x: LocalDate): LocalDate = x.minusDays(1)
+  }
+
+  implicit def localDateOrder: Order[LocalDate] = (x: LocalDate, y: LocalDate) => x.compareTo(y)
+
+  def minus[A](a: Range[A], b: Range[A])(implicit enum: Discrete[A], order: Order[A]): (Option[Range[A]], Option[Range[A]]) = {
+    val left = if (order.gt(b.start, a.start)) Some(Range(a.start, order.min(a.end, enum.pred(b.start)))) else None
+    val right = if (order.lt(b.end, a.end)) Some(Range(order.max(a.start, enum.succ(b.end)), a.end)) else None
+    (left, right)
+  }
+
+  def plus[A] (a:Range[A], b: Range[A])(implicit enum: Discrete[A], order: Order[A]): List[Range[A]] = {
+    val sum = a.+(b)
+    sum._1 :: sum._2.toList
+  }
+
+  def date(year: Int, month: Int, day: Int): LocalDate = LocalDate.of(year, month, day)
 
 }
