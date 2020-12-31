c3f453dd12d229b849e9fe63bd2927212f724762
johaneikelboom <johan@jeikelboom.eu>
2020-12-31 10:44:39 +0100

    Improved Timeline

--- helloScala/src/main/scala/temporal3/TemporalData.scala
+++ helloScala/src/main/scala/temporal3/TemporalData.scala
@@ -1,9 +1,7 @@
 package temporal3
 
-import java.time.LocalDate
-
-import cats.{Applicative, Order}
 import cats.collections.{Discrete, Range}
+import cats.{Applicative, Order}
 
 import scala.annotation.tailrec
 
@@ -12,6 +10,17 @@ object TemporalData {
   trait TimeUnit[T] extends Order[T] with Discrete[T] {
     val MIN: T
     val MAX: T
+
+    def plus (a:Range[T], b: Range[T]): List[Range[T]] = {
+      val sum = a.+(b)(this, this)
+      sum._1 :: sum._2.toList
+    }
+
+    def minus(a: Range[T], b: Range[T]): (Option[Range[T]], Option[Range[T]]) = {
+      val left = if (gt(b.start, a.start)) Some(Range(a.start, min(a.end, pred(b.start)))) else None
+      val right = if (lt(b.end, a.end)) Some(Range(max(a.start, succ(b.end)), a.end)) else None
+      (left, right)
+    }
 
   }
 
@@ -24,25 +33,16 @@ object TemporalData {
 
       def append(that: IntervalData[A]): List[IntervalData[A]] = {
         if (this.value == that.value) {
-          plus(this.range, that.range).map(range => IntervalData(range.start, range.end, value))
+          timeUnit.plus(this.range, that.range).map(range => IntervalData(range.start, range.end, value))
         } else {
-          that :: minus(this.range, that.range)._1.toList.map(range => IntervalData(range.start, range.end, this.value))
+          that :: timeUnit.minus(this.range, that.range)._1.toList.map(range => IntervalData(range.start, range.end, this.value))
         }
       }
 
       override def toString: String = s"$range => $value"
 
 
-      private def plus (a:Range[T], b: Range[T]): List[Range[T]] = {
-        val sum = a.+(b)
-        sum._1 :: sum._2.toList
-      }
 
-    }
-    def minus(a: Range[T], b: Range[T]): (Option[Range[T]], Option[Range[T]]) = {
-      val left = if (timeUnit.gt(b.start, a.start)) Some(Range(a.start, timeUnit.min(a.end, timeUnit.pred(b.start)))) else None
-      val right = if (timeUnit.lt(b.end, a.end)) Some(Range(timeUnit.max(a.start, timeUnit.succ(b.end)), a.end)) else None
-      (left, right)
     }
 
     case class Timeline[A](history: List[IntervalData[A]] = List()) {
@@ -63,7 +63,7 @@ object TemporalData {
 
       def map[B](f: A => B): Timeline[B] = timeLineApplicative.map(this)(f)
 
-      def retroUpdate(timelineElement: IntervalData[A]) ={
+      def retroUpdate(timelineElement: IntervalData[A]): Timeline[A] ={
         def retro[A](timelineElement: IntervalData[A]): Timeline[Option[A]] = {
           val tle1: IntervalData[Option[A]] = IntervalData(timeUnit.MIN, timeUnit.pred(timelineElement.start), None)
           val tle2: IntervalData[Option[A]] = IntervalData(timelineElement.start, timelineElement.end, Some(timelineElement.value))
@@ -93,8 +93,8 @@ object TemporalData {
       private def accumulator[A, B](ff: List[IntervalData[A => B]], fa: List[IntervalData[A]], accu: Timeline[B]): Timeline[B] = {
         (ff, fa) match {
           case ((ff@IntervalData(_, _, _)) :: fftail, (fa@IntervalData(_, _, _)) :: fatail) =>
-            val ffrest: List[IntervalData[A => B]] = minus(ff.range, fa.range)._2.toList.map(range =>IntervalData(range.start, range.end, ff.value)) ::: fftail
-            val farest: List[IntervalData[A]] = minus(fa.range, ff.range)._2.toList.map(range =>IntervalData(range.start, range.end, fa.value)) ::: fatail
+            val ffrest: List[IntervalData[A => B]] = timeUnit.minus(ff.range, fa.range)._2.toList.map(range =>IntervalData(range.start, range.end, ff.value)) ::: fftail
+            val farest: List[IntervalData[A]] = timeUnit.minus(fa.range, ff.range)._2.toList.map(range =>IntervalData(range.start, range.end, fa.value)) ::: fatail
             val accrest: Option[IntervalData[B]] = ff.range.&(fa.range).map(range =>IntervalData(range.start, range.end, ff.value(fa.value)))
             val accuNext: Timeline[B] = accrest.map(e => accu.append(e)).getOrElse(accu)
             accumulator(ffrest, farest, accuNext)
